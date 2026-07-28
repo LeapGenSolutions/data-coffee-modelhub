b6ec06f8926a8dd555d2c49775b926781bfe30c9
hemu1808 <hemu1808@gmail.com>
2026-07-28 11:12:32 -0400

    YAML Build Command update

--- .next/types/app/api/chat/route.ts
+++ .next/types/app/api/chat/route.ts
@@ -1,4 +1,4 @@
-// File: D:\dataco\data-coffee-modelhub\app\api\chat\route.ts
+// File: D:\Repos\data-coffee-modelhub\app\api\chat\route.ts
 import * as entry from '../../../../../app/api/chat/route.js'
 import type { NextRequest } from 'next/server.js'
 
